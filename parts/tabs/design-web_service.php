04c08cc92a59af286457c261929482c0cb0a66b3
Callan Milne <callan.milne@eviratec.com.au>
2018-05-19 19:17:32 -0400

    (fix): WebService design tab

--- parts/tabs/design-web_service.php
+++ parts/tabs/design-web_service.php
@@ -1,22 +1,39 @@
-<?php
-$logic_group_id = get_the_ID();
-$models = DataStudioQuery::getModelsByLogicGroup( $logic_group_id );
-?>
-
 <section class="submodel">
   <header>
-    <h2>Routes</h2>
+    <h2>Paths</h2>
     <span class="spacer"></span>
-    <button id="Toggle_CreateRouteForm">
+    <button id="Toggle_CreatePathForm">
       <span class="material-icons">
         add
       </span>
       <span>
-        Create Route
+        Create Path
       </span>
     </button>
   </header>
   <main>
+    <div class="no-results">
+      <p>You haven't added any paths to this web service.</p>
+    </div>
+  </main>
+</section>
 
+<section class="submodel">
+  <header>
+    <h2>Schema Definitions</h2>
+    <span class="spacer"></span>
+    <button id="Toggle_CreateSchemaDefinitionForm">
+      <span class="material-icons">
+        add
+      </span>
+      <span>
+        Create Schema Definition
+      </span>
+    </button>
+  </header>
+  <main>
+    <div class="no-results">
+      <p>You haven't added any schema definitions to this web service.</p>
+    </div>
   </main>
 </section>
