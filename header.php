a1d6c5365426b136b0bd7342611848dd911ddbc7
Callan Milne <callan.milne@eviratec.com.au>
2018-06-04 23:32:58 -0400

    (fix): <title> tag

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
 
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <title><?php wp_title( '' ); ?></title>
 
     <link href="//www.google-analytics.com" rel="dns-prefetch">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
