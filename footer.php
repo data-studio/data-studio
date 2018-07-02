c363b72d4d1acf20d7268c8a2a4c94dba6352dbf
Callan Milne <callan.milne@eviratec.com.au>
2018-07-02 03:05:40 -0400

    (fix):Google Analytics Tag

--- footer.php
+++ footer.php
@@ -15,14 +15,14 @@
 
     <?php wp_footer(); ?>
 
-    <!-- analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-100013900-2"></script>
     <script>
-    (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-    (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-    l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-    ga('send', 'pageview');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-100013900-2');
     </script>
 
   </body>
